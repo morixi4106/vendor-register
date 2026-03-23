ef2b0d4cf2108388c7f1307dd63f5fb3076c458c
morixi4106 <morixi4106@gmail.com>
2026-03-23 17:09:52 +0900

    Try working webhook code in template route

--- app/routes/webhooks.products.create.jsx
+++ app/routes/webhooks.products.create.jsx
@@ -1,16 +1,11 @@
 import { authenticate } from "../shopify.server";
 
 export const action = async ({ request }) => {
-  try {
-    const { shop, topic, payload } = await authenticate.webhook(request);
+  const { shop, topic, payload } = await authenticate.webhook(request);
 
-    console.log("🔥 webhook hit");
-    console.log(`Received ${topic} webhook for ${shop}`);
-    console.log("📦 product:", payload?.title);
+  console.log("🔥 webhook hit");
+  console.log(`Received ${topic} webhook for ${shop}`);
+  console.log("📦 product:", payload?.title);
 
-    return new Response("OK", { status: 200 });
-  } catch (error) {
-    console.error("❌ webhook error:", error);
-    return new Response("Webhook error", { status: 500 });
-  }
+  return new Response();
 };
