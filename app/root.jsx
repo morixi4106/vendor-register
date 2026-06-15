8e9f2e873a59274f723d9a1b01acaf500fcab393
morixi4106 <morixi4106@gmail.com>
2026-06-15 16:18:17 +0900

    Refresh app favicon cache key

--- app/root.jsx
+++ app/root.jsx
@@ -17,23 +17,23 @@ export default function App() {
           rel="stylesheet"
           href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
         />
-        <link rel="icon" href="/favicon.ico?v=20260605" sizes="any" />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32.png?v=20260605"
+          href="/favicon-32.png?v=20260615"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16.png?v=20260605"
+          href="/favicon-16.png?v=20260615"
         />
+        <link rel="shortcut icon" href="/favicon.ico?v=20260615" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png?v=20260605"
+          href="/apple-touch-icon.png?v=20260615"
         />
         <Meta />
         <Links />
